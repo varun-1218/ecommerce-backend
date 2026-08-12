9f570b8aa497bd713357f7a78f27b091c936a5bf
Varun <svarun1218@gmail.com>
2026-08-13 01:59:25 +0530

    Added Swagger/OpenAPI documentation

---
+++ src/main/java/com/varun/ecommerce/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.varun.ecommerce.config; 
+ 
+import io.swagger.v3.oas.models.OpenAPI; 
+import io.swagger.v3.oas.models.info.Info; 
+import org.springframework.context.annotation.Bean; 
+import org.springframework.context.annotation.Configuration; 
+ 
+@Configuration 
+public class SwaggerConfig { 
+ 
+    @Bean 
+    public OpenAPI customOpenAPI() { 
+        return new OpenAPI() 
+            .info(new Info() 
+                .title("Ecommerce API") 
+                .version("1.0") 
+                .description("Spring Boot Ecommerce Application")); 
+    } 
+} 
